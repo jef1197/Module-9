941f5eb05e53d4b50f59b9fe34b6229877413e28
JefO <jef.ogu@gmail.com>
2023-04-25 20:49:55 -0400

    Update generateMarkdown.js
    generated the readme markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,64 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (!license) {
+    return ' ';
+  }
+}
 
 // TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    if (license == 'None') {
+    return ' ';
+  }
+  return '6. [License](#license)'
+}
 
 // TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license == 'None') {
+    return '\n  ## License\n';
+  }
+}
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  let licenseData = renderLicenseSection(data.license);
+  let badege = renderLicenseBadge(data.license);
+  let licenseLink = renderLicenseLink(data.license);
+  return   `# ${data.title}
+  ## Description
 
-`;
+  ${data.description}
+
+  ##  Table Of Contents
+  1. [Installation](#installation)
+  2. [Usage](#usage)
+  3. [Contribution](#contribution)
+  4. [Test](#test)
+  5. [Questions](#questions)
+  ${licenseLink}
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## Contribution
+
+  ${data.contribution}
+
+  ## Test
+
+  ${data.test}
+  ${licenseData}
+  ## Questions
+  
+  You can contact me reagarding any questions through my email or github below
+  github.com/${data.github}
+  ${data.email}
+  `;
 }
 
 module.exports = generateMarkdown;
